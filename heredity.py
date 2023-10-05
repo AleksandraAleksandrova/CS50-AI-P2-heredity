bcb75ad4e9ca17b02400d93b1243fcaf88fce27d
AleksandraAleksandrova <a.aleksandrova2004@gmail.com>
2023-10-05 17:54:35 +0300

    joint prob func

--- heredity.py
+++ heredity.py
@@ -139,7 +139,53 @@ def joint_probability(people, one_gene, two_genes, have_trait):
         * everyone in set `have_trait` has the trait, and
         * everyone not in set` have_trait` does not have the trait.
     """
-    raise NotImplementedError
+    joint_prob = 1
+    for person in people:
+        person_prob = 1
+        if people[person]["mother"] == None:
+            if person in one_gene:
+                person_prob *= PROBS["gene"][1]
+            elif person in two_genes:
+                person_prob *= PROBS["gene"][2]
+            else:
+                person_prob *= PROBS["gene"][0]
+        else:
+            mother_pass = inheritance(people[person]['mother'], one_gene, two_genes)
+            father_pass = inheritance(people[person]['father'], one_gene, two_genes)
+
+            if person in two_genes:
+                person_prob *= mother_pass * father_pass
+            elif person in one_gene:
+                person_prob *= (1 - mother_pass) * father_pass + mother_pass * (1 - father_pass)
+            else:
+                person_prob *= (1 - mother_pass) * (1 - father_pass)
+
+        if person in have_trait:
+            if person in one_gene:
+                person_prob *= PROBS["trait"][1][True]
+            elif person in two_genes:
+                person_prob *= PROBS["trait"][2][True]
+            else:
+                person_prob *= PROBS["trait"][0][True]
+        else:
+            if person in one_gene:
+                person_prob *= PROBS["trait"][1][False]
+            elif person in two_genes:
+                person_prob *= PROBS["trait"][2][False]
+            else:
+                person_prob *= PROBS["trait"][0][False]
+        
+        joint_prob *= person_prob
+    return joint_prob
+
+
+def inheritance(parent, one_gene, two_genes):
+    if parent in two_genes:
+        return 1 - PROBS['mutation']
+    elif parent in one_gene:
+        return 0.5
+    else:
+        return PROBS['mutation']
 
 
 def update(probabilities, one_gene, two_genes, have_trait, p):
